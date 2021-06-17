bcb01f34895a207bef22c30e7438385b297046ac
FarjanaKamal <farjana.bintay.kamal@gmail.com>
2021-06-17 14:33:17 -0400

    Random number generation project

---
+++ C# Projects/Generating Random Numbers/PPA2/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿
+namespace PPA2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.MinimumNumber = new System.Windows.Forms.TextBox();
+            this.MaximumNumber = new System.Windows.Forms.TextBox();
+            this.NoOfRandom = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.saveFile = new System.Windows.Forms.SaveFileDialog();
+            this.lbxNumbers = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblSum = new System.Windows.Forms.Label();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // MinimumNumber
+            // 
+            this.MinimumNumber.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.MinimumNumber.Location = new System.Drawing.Point(297, 80);
+            this.MinimumNumber.Name = "MinimumNumber";
+            this.MinimumNumber.Size = new System.Drawing.Size(125, 27);
+            this.MinimumNumber.TabIndex = 0;
+            // 
+            // MaximumNumber
+            // 
+            this.MaximumNumber.Location = new System.Drawing.Point(297, 120);
+            this.MaximumNumber.Name = "MaximumNumber";
+            this.MaximumNumber.Size = new System.Drawing.Size(125, 27);
+            this.MaximumNumber.TabIndex = 1;
+            // 
+            // NoOfRandom
+            // 
+            this.NoOfRandom.Location = new System.Drawing.Point(297, 157);
+            this.NoOfRandom.Name = "NoOfRandom";
+            this.NoOfRandom.Size = new System.Drawing.Size(125, 27);
+            this.NoOfRandom.TabIndex = 2;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label.Location = new System.Drawing.Point(98, 80);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(144, 20);
+            this.label.TabIndex = 3;
+            this.label.Text = "Minimum Number:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(98, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Maximum Number:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(66, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(179, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "No of Random Number:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightGreen;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(387, 221);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 52);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Generate and Save Random Number";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbxNumbers
+            // 
+            this.lbxNumbers.FormattingEnabled = true;
+            this.lbxNumbers.ItemHeight = 20;
+            this.lbxNumbers.Location = new System.Drawing.Point(297, 303);
+            this.lbxNumbers.Name = "lbxNumbers";
+            this.lbxNumbers.Size = new System.Drawing.Size(274, 184);
+            this.lbxNumbers.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(66, 303);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Numbers Sum:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(55, 348);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(124, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Numbers Count:";
+            // 
+            // lblSum
+            // 
+            this.lblSum.AutoSize = true;
+            this.lblSum.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblSum.Location = new System.Drawing.Point(197, 303);
+            this.lblSum.Name = "lblSum";
+            this.lblSum.Size = new System.Drawing.Size(0, 20);
+            this.lblSum.TabIndex = 11;
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblCount.Location = new System.Drawing.Point(197, 348);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(0, 20);
+            this.lblCount.TabIndex = 12;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightGreen;
+            this.button2.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(409, 508);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 39);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Show Output";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFile
+            // 
+            this.openFile.FileName = "openFileDialog1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label5.Font = new System.Drawing.Font("Stencil", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(304, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(267, 33);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Random Numbers";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(853, 576);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.lblSum);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbxNumbers);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.NoOfRandom);
+            this.Controls.Add(this.MaximumNumber);
+            this.Controls.Add(this.MinimumNumber);
+            this.Name = "Form1";
+            this.Text = "Farjana Bintay Kamal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox MinimumNumber;
+        private System.Windows.Forms.TextBox MaximumNumber;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox NoOfRandom;
+        private System.Windows.Forms.SaveFileDialog saveFile;
+        private System.Windows.Forms.ListBox lbxNumbers;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblSum;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ C# Projects/Generating Random Numbers/PPA2/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PPA2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int min, max, TotalNumber;
+                if (int.TryParse(MinimumNumber.Text, out min) && int.TryParse(MaximumNumber.Text, out max) && int.TryParse(NoOfRandom.Text, out TotalNumber))
+                {
+                    if (min > 0 && min < max && max < 1000 && TotalNumber > 0)
+                    {
+                        StreamWriter GenerateRandomFile;
+                        saveFile.Filter = "Text File (*.txt)|*.txt|All Files|*.*";
+                        if (saveFile.ShowDialog() == DialogResult.OK)
+                        {
+                            GenerateRandomFile = File.CreateText(saveFile.FileName);
+                            int iteration = 0;
+                            while (iteration < TotalNumber)
+                            {
+                                int RandomNumber = GetRandomNumber();
+                                if (RandomNumber >= min && RandomNumber <= max)
+                                {
+                                    GenerateRandomFile.WriteLine(RandomNumber);
+                                    iteration++;
+                                }
+                            }
+                            GenerateRandomFile.Close();
+                        }
+                        else if (saveFile.ShowDialog() == DialogResult.Cancel)
+                            MessageBox.Show("Please Enter Save");
+                        else
+                            MessageBox.Show("Something is wrong!!!!");
+                    }
+                    
+                    else
+                        MessageBox.Show("Input numbers are not in correct range !");
+
+                }
+                else
+                    MessageBox.Show("Please enter integer input !");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw;
+            }
+            
+        }
+        private int GetRandomNumber()
+        {
+            Random RandomNum = new Random();
+            return RandomNum.Next(1000);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamReader ReadGeneratedRandomFile;
+                if (openFile.ShowDialog() == DialogResult.OK)
+                {
+
+                    ReadGeneratedRandomFile = File.OpenText(openFile.FileName);
+                    int readingNumber, summation=0, counting=0;
+                    while (!ReadGeneratedRandomFile.EndOfStream)
+                    {
+                        int.TryParse(ReadGeneratedRandomFile.ReadLine(), out readingNumber);
+                        lbxNumbers.Items.Add(readingNumber.ToString());
+                        summation += readingNumber;
+                        counting++ ;
+                    }
+                    lblSum.Text = summation.ToString();
+                    lblCount.Text = counting.ToString();
+                    ReadGeneratedRandomFile.Close();
+                }
+                else if (openFile.ShowDialog() == DialogResult.Cancel)
+                    MessageBox.Show("  Please select a file  ");
+                else
+                    MessageBox.Show("Something is wrong!!!!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw;
+            }
+            
+        }
+    }
+}
