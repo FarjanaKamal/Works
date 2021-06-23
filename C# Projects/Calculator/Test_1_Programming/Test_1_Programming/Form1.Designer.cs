2734a3c20facbee021154dd9f2980e776c8cf063
FarjanaKamal <farjana.bintay.kamal@gmail.com>
2021-06-23 13:00:31 -0400

    A program for calculation

---
+++ C# Projects/Calculator/Test_1_Programming/Test_1_Programming/Form1.Designer.cs
@@ -0,0 +1,384 @@
+﻿
+namespace Test_1_Programming
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMain = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.one = new System.Windows.Forms.Button();
+            this.two = new System.Windows.Forms.Button();
+            this.three = new System.Windows.Forms.Button();
+            this.four = new System.Windows.Forms.Button();
+            this.five = new System.Windows.Forms.Button();
+            this.six = new System.Windows.Forms.Button();
+            this.seven = new System.Windows.Forms.Button();
+            this.eight = new System.Windows.Forms.Button();
+            this.nine = new System.Windows.Forms.Button();
+            this.zero = new System.Windows.Forms.Button();
+            this.add = new System.Windows.Forms.Button();
+            this.sub = new System.Windows.Forms.Button();
+            this.mul = new System.Windows.Forms.Button();
+            this.div = new System.Windows.Forms.Button();
+            this.mod = new System.Windows.Forms.Button();
+            this.total = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Submit = new System.Windows.Forms.Button();
+            this.Load = new System.Windows.Forms.RadioButton();
+            this.Save = new System.Windows.Forms.RadioButton();
+            this.History = new System.Windows.Forms.ListBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtMain
+            // 
+            this.txtMain.Location = new System.Drawing.Point(23, 31);
+            this.txtMain.Name = "txtMain";
+            this.txtMain.Size = new System.Drawing.Size(320, 27);
+            this.txtMain.TabIndex = 0;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(24, 79);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(49, 20);
+            this.label.TabIndex = 1;
+            this.label.Text = "Result";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(104, 78);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(239, 27);
+            this.txtResult.TabIndex = 2;
+            // 
+            // one
+            // 
+            this.one.Location = new System.Drawing.Point(22, 124);
+            this.one.Name = "one";
+            this.one.Size = new System.Drawing.Size(74, 38);
+            this.one.TabIndex = 3;
+            this.one.Text = "1";
+            this.one.UseVisualStyleBackColor = true;
+            this.one.Click += new System.EventHandler(this.one_Click);
+            // 
+            // two
+            // 
+            this.two.Location = new System.Drawing.Point(103, 124);
+            this.two.Name = "two";
+            this.two.Size = new System.Drawing.Size(74, 38);
+            this.two.TabIndex = 4;
+            this.two.Text = "2";
+            this.two.UseVisualStyleBackColor = true;
+            this.two.Click += new System.EventHandler(this.one_Click);
+            // 
+            // three
+            // 
+            this.three.Location = new System.Drawing.Point(184, 125);
+            this.three.Name = "three";
+            this.three.Size = new System.Drawing.Size(74, 38);
+            this.three.TabIndex = 5;
+            this.three.Text = "3";
+            this.three.UseVisualStyleBackColor = true;
+            this.three.Click += new System.EventHandler(this.one_Click);
+            // 
+            // four
+            // 
+            this.four.Location = new System.Drawing.Point(22, 186);
+            this.four.Name = "four";
+            this.four.Size = new System.Drawing.Size(74, 38);
+            this.four.TabIndex = 6;
+            this.four.Text = "4";
+            this.four.UseVisualStyleBackColor = true;
+            this.four.Click += new System.EventHandler(this.one_Click);
+            // 
+            // five
+            // 
+            this.five.Location = new System.Drawing.Point(106, 186);
+            this.five.Name = "five";
+            this.five.Size = new System.Drawing.Size(74, 38);
+            this.five.TabIndex = 7;
+            this.five.Text = "5";
+            this.five.UseVisualStyleBackColor = true;
+            this.five.Click += new System.EventHandler(this.one_Click);
+            // 
+            // six
+            // 
+            this.six.Location = new System.Drawing.Point(187, 187);
+            this.six.Name = "six";
+            this.six.Size = new System.Drawing.Size(74, 38);
+            this.six.TabIndex = 8;
+            this.six.Text = "6";
+            this.six.UseVisualStyleBackColor = true;
+            this.six.Click += new System.EventHandler(this.one_Click);
+            // 
+            // seven
+            // 
+            this.seven.Location = new System.Drawing.Point(22, 252);
+            this.seven.Name = "seven";
+            this.seven.Size = new System.Drawing.Size(74, 38);
+            this.seven.TabIndex = 9;
+            this.seven.Text = "7";
+            this.seven.UseVisualStyleBackColor = true;
+            this.seven.Click += new System.EventHandler(this.one_Click);
+            // 
+            // eight
+            // 
+            this.eight.Location = new System.Drawing.Point(104, 251);
+            this.eight.Name = "eight";
+            this.eight.Size = new System.Drawing.Size(74, 38);
+            this.eight.TabIndex = 10;
+            this.eight.Text = "8";
+            this.eight.UseVisualStyleBackColor = true;
+            this.eight.Click += new System.EventHandler(this.one_Click);
+            // 
+            // nine
+            // 
+            this.nine.Location = new System.Drawing.Point(186, 252);
+            this.nine.Name = "nine";
+            this.nine.Size = new System.Drawing.Size(74, 38);
+            this.nine.TabIndex = 11;
+            this.nine.Text = "9";
+            this.nine.UseVisualStyleBackColor = true;
+            this.nine.Click += new System.EventHandler(this.one_Click);
+            // 
+            // zero
+            // 
+            this.zero.Location = new System.Drawing.Point(105, 316);
+            this.zero.Name = "zero";
+            this.zero.Size = new System.Drawing.Size(74, 38);
+            this.zero.TabIndex = 12;
+            this.zero.Text = "0";
+            this.zero.UseVisualStyleBackColor = true;
+            this.zero.Click += new System.EventHandler(this.one_Click);
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(270, 125);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(74, 38);
+            this.add.TabIndex = 13;
+            this.add.Text = "+";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.AutoSizeChanged += new System.EventHandler(this.add_Click);
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // sub
+            // 
+            this.sub.Location = new System.Drawing.Point(271, 192);
+            this.sub.Name = "sub";
+            this.sub.Size = new System.Drawing.Size(74, 38);
+            this.sub.TabIndex = 14;
+            this.sub.Text = "-";
+            this.sub.UseVisualStyleBackColor = true;
+            this.sub.AutoSizeChanged += new System.EventHandler(this.add_Click);
+            this.sub.Click += new System.EventHandler(this.add_Click);
+            // 
+            // mul
+            // 
+            this.mul.Location = new System.Drawing.Point(270, 252);
+            this.mul.Name = "mul";
+            this.mul.Size = new System.Drawing.Size(74, 38);
+            this.mul.TabIndex = 15;
+            this.mul.Text = "*";
+            this.mul.UseVisualStyleBackColor = true;
+            this.mul.AutoSizeChanged += new System.EventHandler(this.add_Click);
+            this.mul.Click += new System.EventHandler(this.add_Click);
+            // 
+            // div
+            // 
+            this.div.Location = new System.Drawing.Point(269, 317);
+            this.div.Name = "div";
+            this.div.Size = new System.Drawing.Size(74, 38);
+            this.div.TabIndex = 16;
+            this.div.Text = "/";
+            this.div.UseVisualStyleBackColor = true;
+            this.div.AutoSizeChanged += new System.EventHandler(this.add_Click);
+            this.div.Click += new System.EventHandler(this.add_Click);
+            // 
+            // mod
+            // 
+            this.mod.Location = new System.Drawing.Point(269, 371);
+            this.mod.Name = "mod";
+            this.mod.Size = new System.Drawing.Size(74, 38);
+            this.mod.TabIndex = 17;
+            this.mod.Text = "%";
+            this.mod.UseVisualStyleBackColor = true;
+            this.mod.AutoSizeChanged += new System.EventHandler(this.add_Click);
+            this.mod.Click += new System.EventHandler(this.add_Click);
+            // 
+            // total
+            // 
+            this.total.Location = new System.Drawing.Point(22, 372);
+            this.total.Name = "total";
+            this.total.Size = new System.Drawing.Size(236, 37);
+            this.total.TabIndex = 18;
+            this.total.Text = "Total";
+            this.total.UseVisualStyleBackColor = true;
+            this.total.Click += new System.EventHandler(this.total_Click);
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(384, 366);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(150, 44);
+            this.clear.TabIndex = 19;
+            this.clear.Text = "Clear";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Submit);
+            this.groupBox1.Controls.Add(this.Load);
+            this.groupBox1.Controls.Add(this.Save);
+            this.groupBox1.Controls.Add(this.History);
+            this.groupBox1.Location = new System.Drawing.Point(351, 30);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(215, 324);
+            this.groupBox1.TabIndex = 20;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "History";
+            // 
+            // Submit
+            // 
+            this.Submit.Location = new System.Drawing.Point(32, 274);
+            this.Submit.Name = "Submit";
+            this.Submit.Size = new System.Drawing.Size(150, 44);
+            this.Submit.TabIndex = 3;
+            this.Submit.Text = "Submit";
+            this.Submit.UseVisualStyleBackColor = true;
+            this.Submit.Click += new System.EventHandler(this.Submit_Click);
+            // 
+            // Load
+            // 
+            this.Load.AutoSize = true;
+            this.Load.Location = new System.Drawing.Point(15, 236);
+            this.Load.Name = "Load";
+            this.Load.Size = new System.Drawing.Size(63, 24);
+            this.Load.TabIndex = 2;
+            this.Load.TabStop = true;
+            this.Load.Text = "Load";
+            this.Load.UseVisualStyleBackColor = true;
+            // 
+            // Save
+            // 
+            this.Save.AutoSize = true;
+            this.Save.Location = new System.Drawing.Point(15, 198);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(61, 24);
+            this.Save.TabIndex = 1;
+            this.Save.TabStop = true;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            // 
+            // History
+            // 
+            this.History.FormattingEnabled = true;
+            this.History.ItemHeight = 20;
+            this.History.Location = new System.Drawing.Point(14, 27);
+            this.History.Name = "History";
+            this.History.Size = new System.Drawing.Size(184, 164);
+            this.History.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 459);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.total);
+            this.Controls.Add(this.mod);
+            this.Controls.Add(this.div);
+            this.Controls.Add(this.mul);
+            this.Controls.Add(this.sub);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.zero);
+            this.Controls.Add(this.nine);
+            this.Controls.Add(this.eight);
+            this.Controls.Add(this.seven);
+            this.Controls.Add(this.six);
+            this.Controls.Add(this.five);
+            this.Controls.Add(this.four);
+            this.Controls.Add(this.three);
+            this.Controls.Add(this.two);
+            this.Controls.Add(this.one);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.txtMain);
+            this.Name = "Form1";
+            this.Text = "Farjana Bintay Kamal";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMain;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button one;
+        private System.Windows.Forms.Button two;
+        private System.Windows.Forms.Button three;
+        private System.Windows.Forms.Button four;
+        private System.Windows.Forms.Button five;
+        private System.Windows.Forms.Button six;
+        private System.Windows.Forms.Button seven;
+        private System.Windows.Forms.Button eight;
+        private System.Windows.Forms.Button nine;
+        private System.Windows.Forms.Button zero;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button sub;
+        private System.Windows.Forms.Button mul;
+        private System.Windows.Forms.Button div;
+        private System.Windows.Forms.Button mod;
+        private System.Windows.Forms.Button total;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button Submit;
+        private System.Windows.Forms.RadioButton Load;
+        private System.Windows.Forms.RadioButton Save;
+        private System.Windows.Forms.ListBox History;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ C# Projects/Calculator/Test_1_Programming/Test_1_Programming/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace Test_1_Programming
+{
+    public partial class Form1 : Form
+    {
+        String Number1Str, Number2Str,operation;
+        bool OperationFlag=false;
+        int Result;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void SetNumber(string num)
+        {
+
+            if (!OperationFlag)
+            {
+                Number1Str += num;
+                
+            }
+            else
+            {
+                Number2Str += num;
+                
+            }
+               
+        }
+
+        private bool SetOperation(string op)
+        {
+
+            if (string.IsNullOrEmpty(Number1Str))
+                MessageBox.Show("Please Select the first number first, then select the operation");
+            else if (!string.IsNullOrEmpty(Number2Str))
+                MessageBox.Show("Invalid Selection, Clear current Calculation first and start over.");
+            else if (OperationFlag)
+            {
+                txtMain.Text = txtMain.Text[0..^1];
+                operation = op;
+                OperationFlag = true;
+                
+            }
+            else
+            {
+                operation = op;
+                OperationFlag = true;
+                
+            }
+            return OperationFlag;
+
+        }
+        private int Calculate(int Number1, int Number2)
+        {
+            
+            switch (operation)
+            {
+                case "+":                  
+                        Result = Number1 + Number2;
+                        break;
+                case "-":
+                    Result = Number1 - Number2;
+                    break;
+                case "*":
+                    Result = Number1 * Number2;
+                    break;
+                case "/":
+                    Result = Number1 / Number2;
+                    break;
+                case "%":
+                    Result = Number1 % Number2;
+                    break;
+                default:
+                    break;
+            }
+            return Result;
+        }
+        private void total_Click(object sender, EventArgs e)
+        {
+            int num1, num2;
+            int.TryParse(Number1Str, out num1);
+            int.TryParse(Number2Str, out num2);
+            txtResult.Text = (Calculate(num1, num2).ToString());
+            string history = txtMain.Text + " = " + txtResult.Text;
+            History.Items.Add(history);
+        }
+        private void UpdateGUI(string val)
+        {
+            txtMain.Text += val;
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            Number1Str = null;
+            Number2Str = null;
+            operation = null;
+            OperationFlag = false;
+            Result=0;
+            txtMain.Text = "";
+            txtResult.Text = "";
+
+        }
+
+        private void Submit_Click(object sender, EventArgs e)
+        {
+           
+
+            if (Save.Checked)
+            {
+                StreamWriter submission;
+                saveFileDialog1.Filter = "Text File (*.txt)|*.txt|All Files|*.*";
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    submission = File.CreateText(saveFileDialog1.FileName);
+                    foreach (var item in History.Items)
+                    {
+                        submission.WriteLine(item);
+                    }
+                    submission.Close();
+                }
+                else if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                    MessageBox.Show("Please Enter Save");
+                else
+                    MessageBox.Show("Something is wrong!!!!");
+            }
+            else if (Load.Checked)
+            {
+                StreamReader submission;
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    submission = File.OpenText(openFileDialog1.FileName);
+                    History.Items.Clear();
+                    while (!submission.EndOfStream)
+                    {
+                        History.Items.Add((submission.ReadLine()).ToString());
+                    }
+                    submission.Close();
+                }
+                else if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                    MessageBox.Show("Please Choose a file");
+                else
+                    MessageBox.Show("Something is wrong!!!!");
+            }
+            else
+            {
+                StreamWriter submission;
+                submission = File.AppendText(@"C:\Test1Result\CalculationHistory.txt");
+                foreach (var item in History.Items)
+                {
+                    submission.WriteLine(item);
+                }
+                submission.Close();
+            }
+            
+            
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            string ArithmeticOp;
+            
+            ArithmeticOp= ((Button)sender).Text;
+            if (SetOperation(ArithmeticOp))
+                UpdateGUI(ArithmeticOp);
+
+           // UpdateGUI(a)
+            
+        }
+
+        
+        private void one_Click(object sender, EventArgs e)
+        {
+            string InputNumber;
+            InputNumber=((Button)sender).Text;
+            SetNumber(InputNumber);
+            UpdateGUI(InputNumber);
+        }
+
+       
+
+        
+    }
+}
